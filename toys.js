6b2ee30ae610bf4cb26a4d7a736a8c3786b8696e
SOFT ICODE <141865007+Soft-icode@users.noreply.github.com>
2023-11-16 12:32:17 +0100

    Add files via upload

---
+++ toys.js
@@ -0,0 +1,60 @@
+
+/* welcome back section*/
+let welcomeEl = document.getElementById("welcome-el")
+
+let greeting = "Welcomeback "
+ const greet = () =>{
+welcomeEl.innerText = greeting  + "👋"
+ }
+ setTimeout(greet, 3000)
+
+/*count section*/
+const counts = document.querySelectorAll('.count')
+const speed = 50
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count)
+            setTimeout(upDate, 50)
+        }else{
+            counter.innerText = target
+            counter.innerText += "+"
+        }
+    }
+    upDate()
+})
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
